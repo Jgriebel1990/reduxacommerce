d8131c4e7cec53ce641879a7e1c299539067b66d
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-07 19:41:54 -0500

    satisfied the requirements in my store but its just a filler function imported provider and store into index.js, added the redux dev tool

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import store from './store';
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
