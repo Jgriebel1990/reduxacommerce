306104f1e1e7f6369c390c609744cbd3ae2c1b1d
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-09 18:59:06 -0500

    created initialState for my search bar and created a searchbar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import ProductList from './ProductList';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import ProductList from "./ProductList";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ProductList products={this.props.products}/>
+        <ProductList products={this.props.products} />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return state;
-}
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     dispatch: dispatch
-  }
-}
+  };
+};
 export default connect(mapStateToProps, mapDispatchToProps)(App);
