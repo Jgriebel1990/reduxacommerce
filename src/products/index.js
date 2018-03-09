b884ef5ea1e3472473697345961ef31b16b9a88e
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-08 20:51:29 -0500

    implimented the add_item action type to Product

--- src/products/index.js
+++ src/products/index.js
@@ -1,28 +1,44 @@
-
 const initialState = [
-    {
-        imgSrc: 'https://via.placeholder.com/100x100',
-        name: 'Javelin (caution)',
-        price: 500.00,
-        count: 0
-    },
-    {
-        imgSrc: 'https://via.placeholder.com/100x100',
-        name: 'Snail',
-        price: 3.99,
-        count: 0
-    },
-    {
-        imgSrc: 'https://via.placeholder.com/100x100',
-        name: 'Rug',
-        price: 93.47,
-        count: 0
-    },
-    
+  {
+    imgSrc: "https://via.placeholder.com/100x100",
+    name: "Javelin (caution)",
+    price: 500.0,
+    count: 0
+  },
+  {
+    imgSrc: "https://via.placeholder.com/100x100",
+    name: "Snail",
+    price: 3.99,
+    count: 0
+  },
+  {
+    imgSrc: "https://via.placeholder.com/100x100",
+    name: "Rug",
+    price: 93.47,
+    count: 0
+  }
 ];
-const reducer = (state=initialState, action) => {
-    console.log('product stuffs');
-    return state;
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      const position = action.payload.value;
+      const selectedItem = state[position];
+      const before = state.slice(0, position);
+      const after = state.slice(position + 1);
+      const newProduct = {
+        ...selectedItem,
+        count: selectedItem.count + 1
+      };
+      return [
+        ...before,
+        newProduct,
+        ...after
+      ];
+    case "REMOVE_ITEM":
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default reducer;
