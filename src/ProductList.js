5c57cae7ab793d2e0a1ccbcb73a771c4821cd508
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-08 20:00:41 -0500

    reorganized an individual product component to be in a seperate file

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,17 +1,5 @@
 import React from "react";
-
-const Product = props => {
-  return (
-    <li>
-      <img src={props.imgSrc} alt={props.name} />
-      <p>{props.name}</p>
-      <p>${props.price}</p>
-      <button onClick={() => console.log("+")}>+</button>
-      <p>{props.count}</p>
-      <button onClick={() => console.log("-")}>-</button>
-    </li>
-  );
-};
+import Product from './Product'
 const ProductList = props => {
   const ProductItems = props.products.map((product, index) => (
     <Product {...product} key={index} />
