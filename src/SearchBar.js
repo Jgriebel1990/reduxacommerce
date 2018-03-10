c366b5766cef0f3d80a370beaa469c4c026c31e2
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-09 19:32:36 -0500

    connected search bar with appropriate state and dispatch props

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const SearchBar = props => {
     return(
-        <div>search</div>
+        <div>
+            <input type="text" 
+                    onChange={(e) => props.updateSearch(e.target.value)}
+                    value={props.searchTerm}/>
+        </div>
     );
 }
 
-export default SearchBar;
+const mapStateToProps = state => {
+    return {
+        searchTerm: state.search
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        updateSearch: term => dispatch({type: 'UPDATE_SEARCH', payload: {value: term}})
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
