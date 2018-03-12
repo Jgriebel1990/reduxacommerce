45d663644eabc2ffe2bbaa074809cb899dfd627d
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-12 19:25:13 -0400

    import CartButton and CartList into my app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import ProductList from "./ProductList";
 import SearchBar from './SearchBar';
 import CartButton from './CartButton';
+import CartList from './CartList';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <SearchBar />
         <ProductList searchTerm={this.props.search}
                      products={this.props.products} />
+        <CartList />
       </div>
     );
   }
---
+++ src/CartList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const CartList = props => {
+    return (
+        <div>
+            cart list ya
+        </div>
+    );
+}
+
+export default CartList;
